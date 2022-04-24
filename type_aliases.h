c34d624701b83ec228859cb559f03d674559d694
David Theese <dtheese@rocky-1.home.theese.com>
2022-04-24 13:59:18 -0700

    Don't use unordered containers

--- type_aliases.h
+++ type_aliases.h
@@ -5,17 +5,16 @@
 #include <cstddef>
 #include <functional>
 #include <map>
+#include <set>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 
 using namespace std;
 
 using my_uint_t = long unsigned int;
 using entropy_t = long double;
-using bin_item_count_map_t = unordered_map<string, my_uint_t>;
-using bin_probability_map_t = unordered_map<string, entropy_t>;
+using bin_item_count_map_t = map<string, my_uint_t>;
+using bin_probability_map_t = map<string, entropy_t>;
 using entropy_words_map_t = multimap<entropy_t, string, greater<entropy_t>>;
-using word_list_t = unordered_set<string>;
+using word_list_t = set<string>;
 
 #endif
